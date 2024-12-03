3a20adbab6469ce68a06e3fab6d04aaa834ca0b4
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2024-12-03 17:23:50 +0000

    Completed five tasks

---
+++ main.js
@@ -0,0 +1,79 @@
+//Task 1:
+const favoriteFoods = ["Cheese", "Chocolate", "Olives", "Anchovies"];
+
+const favoriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"]
+//1. Favorite Songs
+//Tiny Dancer
+//Running Up That Hill
+//Don't Stop Moving
+//Tragedy
+//Crazy Horses
+
+const profitSevenDayPyramindScheme = [1, 2, 4, 8, 16, 32, 64]
+//2. Profit From First Seven Days of the School of Code's Penny Doubling Pyramid Scheme 💰💰💰
+//1
+//2
+//4
+//8
+//16
+//32
+//64
+
+const friends = []
+// Friends
+//Name is Chris; special skill is changing lives.
+//Name is Lizzie; special skill is being friendly.
+//Name is Ben; special skill is code rapping.
+//Name is Tao; special skill is hand raising.
+//Name is Liz; special skill is crochet.
+//Name is Patrick; special skill is Call of Duty.
+//Name is Tim; special skill is analogies.
+//Name is James; special skill is friendship.
+//Name is Joseph; special skill is being positive.
+//Name is Max; special skill is papier-mâché.
+
+//Task 2:
+let options = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  console.log(options[10]) // month I was born
+  console.log(options[8]) // month my mum was born
+  console.log(options[7]) // my favourite month
+;
+
+//Task 3:
+let englishNumbers = ["one", "two", "three", "four", "five"];
+
+let index = 0;
+
+while (index < englishNumbers.length) {
+    console.log(englishNumbers[index]); index++;
+} 
+
+//Task 4:
+for (let i = 0; i < englishNumbers.length; i++) {
+   console.log(englishNumbers[i])
+}
+//Declare an index variable and assign it the value of 0.
+//In a while loop, console.log the array item at the current index and add 1 to the index variable.
+//Have the while loop run until the index is equal to or greater than the array's length property.
+
+//Task 5:
+let words = ["ben", "ha", "spla", "fa", "ca", "dra"];
+for (let i = 0; i < words.length; i++ ) {
+    console.log(words[i] + "t") } 
+
+//Use a for loop to iterate over the words array and add the letter t to the end of each word ("ben" becomes "bent", "ha" becomes "hat", etc.).
+
